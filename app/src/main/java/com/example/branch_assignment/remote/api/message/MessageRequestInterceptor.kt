4aef8a3f1f20de1e5d7d22d1ba79e0c658b9dd22
pranav <pranavvyas2003@gmail.com>
2023-09-08 15:39:13 +0530

    use run blocking to retrieve token

--- app/src/main/java/com/example/branch_assignment/remote/api/message/MessageRequestInterceptor.kt
+++ app/src/main/java/com/example/branch_assignment/remote/api/message/MessageRequestInterceptor.kt
@@ -1,6 +1,7 @@
 package com.example.branch_assignment.remote.api.message
 
 import com.example.branch_assignment.utils.TokenManager
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
@@ -12,8 +13,10 @@ class MessageRequestInterceptor @Inject constructor() : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().newBuilder()
 
-//        val token = tokenManager.getToken()
-        request.addHeader("Authorization", "Bearer ")
+        val token = runBlocking {
+            tokenManager.getToken()
+        }
+        request.addHeader("X-Branch-Auth-Token", token!!)
         return chain.proceed(request.build())
     }
 }
